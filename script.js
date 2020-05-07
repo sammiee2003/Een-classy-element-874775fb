74f01f0b3eee56c6a7431c4e8077427d6bda1c0b
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-05-07 11:52:21 +0200

    level 2 opdr 3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function Functie(){
-    document.querySelector("#demo").innerText = "change it up";
+    document.querySelector("#demo").innerText = "een classy text";
+    document.querySelector("#demo").classList.add("style");
 }
